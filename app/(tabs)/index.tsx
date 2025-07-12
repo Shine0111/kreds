9a7dcf27c11a1919a29e3dbcd5092936f6071ca4
Shine Ran <ranshine9@gmail.com>
2025-07-12 14:57:49 -0400

    text recoginition implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
+import TextRecognition from "@react-native-ml-kit/text-recognition";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
-import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useRef, useState } from "react";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [photoUri, setPhotoUri] = useState<string>("");
+  const cameraRef = useRef<CameraView>(null);
 
   if (!permission) {
     return <View />;
@@ -24,9 +35,28 @@ export default function Index() {
   const toggleCameraFacing = () => {
     setFacing((current) => (current === "back" ? "front" : "back"));
   };
+
+  const takePicture = async () => {
+    if (!cameraRef.current) return;
+
+    const photo = await cameraRef.current.takePictureAsync();
+    setPhotoUri(photo.uri);
+    setIsProcessing(true);
+
+    try {
+      const result = await TextRecognition.recognize(photo.uri);
+      console.log("Text recognition result:", result);
+    } catch (error) {
+      console.error("Error during text recognition:", error);
+      alert("Failed to recognize text. Please try again.");
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.flipButton}
@@ -36,6 +66,13 @@ export default function Index() {
           </TouchableOpacity>
         </View>
       </CameraView>
+      <View>
+        <Pressable
+          onPress={takePicture}
+          style={styles.captureButton}
+        ></Pressable>
+        {isProcessing && <Text style={styles.text}>Processing...</Text>}
+      </View>
     </View>
   );
 }
@@ -75,5 +112,13 @@ const styles = StyleSheet.create({
   message: {
     textAlign: "center",
     paddingBottom: 10,
+  },
+  captureButton: {
+    backgroundColor: "white",
+    width: 75,
+    height: 75,
+    borderRadius: "50%",
+    borderWidth: 5,
+    borderColor: "#595a59",
   },
 });
